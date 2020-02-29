4eabbdf13f772664748905a2592f98f4d8a3c7e8
Theodore Gautama Chandra <38230742+theo123490@users.noreply.github.com>
2020-02-29 17:34:03 +0700

    MODIFY main by changing input back to glider gun and changing delay to 0.2

--- lib/main.rb
+++ lib/main.rb
@@ -5,13 +5,13 @@ class ReaderModule
   include Reader
 end
 
-file_location = File.join(File.dirname(__FILE__), 'pulsar.csv')
+file_location = File.join(File.dirname(__FILE__), 'input.csv')
 live_location = ReaderModule.new.csv_to_location(file_location)
 
 world = GameOfLife::World.new
 
 live_location.each do |life|
-  world.add_life(life[0], life[1])
+  world.add_life(life[1], life[0])
 end
 
 loop do
@@ -20,5 +20,5 @@ loop do
   world.update_life_array
   $stdout.flush
   puts '********************************************'
-  sleep(0.5)
+  sleep(0.2)
 end
